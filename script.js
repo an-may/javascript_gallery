84bcd6b57628909888ccc204492e4d55420d77f0
カマダマユキ <131324301+an-may@users.noreply.github.com>
2024-10-15 10:21:02 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// JavaScript
+//console.log('Hello world!');
+
+const main = document.querySelector('#main-image');
+const thumbs = document.querySelectorAll('.thumb');
+
+// 確認
+//console.log(main);
+//console.log(thumbs);
+
+//オンクリック
+//thumbs.forEach(function(item, index) {
+   // item.onclick = function() {
+      // console.log(this.dataset.image);
+     // main.src = this.dataset.image;
+    //}
+ // });
+
+ const text = document.querySelector('#caption');
+
+// マウスオーバー
+thumbs.forEach(function(item, index) {
+    item.onmouseover = function() {
+      //console.log(this.dataset.image);
+      main.src = this.dataset.image;
+      text.textContent = this.title;
+    }
+  });
